cd971e3181f148198ce8d4e0763af9d7ee7893a9
thaianhtai <thaianhtai167@gmail.com>
2025-02-26 10:46:00 +0700

    Cập nhật authentication middleware và xóa conversation.service.js

--- server/src/middlewares/authentication.js
+++ server/src/middlewares/authentication.js
@@ -1,24 +1,31 @@
-const authenticationUser = (req, res, next) => {
-    const authorizationHeader = req.headers['authorization']; // Lấy header authorization
+const JWT = require("jsonwebtoken");
+const asyncHandle = require("../helper/asyncHandle");
+const { ErrorResponse } = require("../core/error.response");
+const verifyAccessToken = require("../utils/auth/verifyAccessToken");
+const { findUserById } = require("../models/repositories/user.repo");
+
+const HEADER = {
+    AUTHORIZATION: "authorization",
+}
+
+const authentication = asyncHandle(async (req, res, next) => {
+    const authorizationHeader = req.headers[HEADER.AUTHORIZATION]
     if (authorizationHeader) {
-        const accessToken = authorizationHeader.split(' ')[1]; // Tách access token ra
-        try {
-            // Giải mã access token và kiểm tra token hợp lệ
-            const decodedToken = verifyAccessToken(accessToken); // Giả sử verifyAccessToken là hàm của bạn
-            req.userId = decodedToken._id; // Lưu thông tin userId vào req
-            next(); // Tiếp tục với request
-        } catch (err) {
-            return res.status(401).json({
-                success: false,
-                message: "Invalid or expired token"
-            });
-        }
+        const accessToken = authorizationHeader.split(' ')[1];
+        // Giải mã access token sử dụng chữ ký được chia sẻ giữa máy chủ và client
+        const decodedToken = verifyAccessToken(accessToken);
+        // Tìm kiếm user liên quan đến access token
+        const user = await findUserById(decodedToken._id)
+        if (!user) throw new ErrorResponse("invalid user", 201)
+        // Lưu thông tin user vào request object để sử dụng cho các request handlers sau này
+        req.userId = user._id;
+        next(); // Cho phép request đi tiếp
     } else {
         return res.status(401).json({
             success: false,
-            message: "Authorization header missing"
+            message: "Require authentication"
         });
     }
-};
+});
 
-module.exports = authenticationUser;
+module.exports = authentication;

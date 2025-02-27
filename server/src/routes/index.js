eb16564c82a29008c275d6fbca2c850e0de4b353
thaianhtai <thaianhtai167@gmail.com>
2025-02-27 12:15:15 +0700

    Lưu thay đổi trước khi pull

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -15,6 +15,6 @@ router.use("/v1/api/shippingCompany", require("./shippingCompany/index"))
 router.use("/v1/api/voucher", require("./voucher/index"))
 router.use("/v1/api/favorite", require("./favoriteProduct/index"));
 router.use("/v1/api/notification", require("./notification/index"))
-// router.use("/v1/api/inventory", require("./inventory/index"))
+router.use("/v1/api/supplier", require("./supplier/index"))
 
 module.exports = router

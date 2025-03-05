4c5025cc7dba9c97297487610b0926344d1a2dff
unknown <datp1907@gmail.com>
2025-03-05 12:11:53 +0700

    up code

--- server/src/models/repositories/cart.repo.js
+++
@@ -1,9 +0,0 @@
-const { convertToObjectIdMongodb } = require("../../utils")
-const Cart = require("../cart.model")
-
-
-const findCartById = async (cartId) => {
-    return await Cart.findOne({ _id: convertToObjectIdMongodb(cartId), cart_status: "active" }).lean()
-}
-
-module.exports = findCartById
